99765dc900eccc3213411e4919c8797faee91f72
Francois Van Graan <fcvgraan@gmail.com>
2020-10-31 11:35:39 +0200

    get SA holidays can calculate business time. updated readme

--- README.md
+++ README.md
@@ -2,11 +2,15 @@
 flask api that runs in google cloud functions for recomed recruitment assignment
 
 * Done: simple logic to get request parameters
+* Done: figure out public holidays dynamically with https://pypi.org/project/holidays/
+* Done: calculate business time with https://pypi.org/project/businesstimedelta/
 
-* TODO: figure out ISO-8601 date comparison
-* TODO: figure out public holidays dynamically
-* TODO: calculate business seconds
+* TODO: figure out ISO-8601 date comparison and processing
 * TODO: automated testing
 * TODO: automated deployment
 * TODO: Authentication (will be nice to have)
-* TODO: Persistent storage (will be nice to have)
+* TODO: Persistent storage (will be nice to have
+
+
+
+
--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+import datetime
+import pytz
+import businesstimedelta
+import holidays as pyholidays
+
 def http_trigger(request):
     """ HTTP Cloud Function
     Arg: request (flask.Request)
@@ -5,4 +10,26 @@ def http_trigger(request):
     """
     start_time = request.args.get('start_time')
     end_time = request.args.get('end_time')
-    return 'Start time is: {}, and end time is: {}'.format(start_time,end_time)
+
+    diff = calculateBusinessTime()
+    return 'diff is: {}'.format(diff)
+
+def calculateBusinessTime():
+
+    # Define a working day
+    workday = businesstimedelta.WorkDayRule(
+    start_time=datetime.time(8),
+    end_time=datetime.time(17),
+    working_days=[0, 1, 2, 3, 4])
+
+    sa_holidays = pyholidays.ZA()
+    holidays = businesstimedelta.HolidayRule(sa_holidays)
+    businesshrs = businesstimedelta.Rules([workday, holidays])
+
+    # Thursday 24 September 2020 was a public holiday in South Africa
+    start = datetime.datetime(2020, 9, 23, 8, 0, 0)
+    end = datetime.datetime(2020, 9, 25, 17, 0, 0)
+
+    bhours = businesshrs.difference(start, end).hours
+
+    return bhours
